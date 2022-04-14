419694388661e70c041ae0e4c617d23afceb2152
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-04-14 10:47:59 -0400

    added models

---
+++ models/User.models.js
@@ -0,0 +1,15 @@
+const {Schema, model} = require('mongoose')
+
+const userSchema =  new Schema({
+    user: {
+        type: Name,
+        required: true
+    }
+}, {
+    timeseries: true,
+    timestamps: true
+})
+
+const User =  model("User", userSchema)
+
+module.exports = User
